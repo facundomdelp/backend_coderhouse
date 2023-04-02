7a38913c307221aa2a721c99c3024dcdf1145da7
facundomdelp <facundomdelp@gmail.com>
2023-04-02 12:29:17 -0300

    desafio 2

--- proyecto.js
+++ proyecto.js
@@ -1,10 +1,27 @@
+const fs = require('fs')
+
 class ProductManager {
 
-  constructor() {
+  constructor(collectionName) {
+    this.path = `./collection/${collectionName}.json`
     this.products = []
+    ProductManager.createCollectionFolder()
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  static createCollectionFolder = () => {
+    fs.promises.mkdir('./collection', { recursive: true })
+  }
+
+  static getCollection = async (path) => {
+    try {
+      return JSON.parse(await fs.promises.readFile(path, 'utf-8'))
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
     if (this.products.find(product => product.code === code)) return console.log('Not valid - Repeated code')
     const params = [title, description, price, thumbnail, code, stock]
     if (params.includes(undefined)) return console.log('Not Valid - Enter all the fields')
@@ -17,54 +34,47 @@ class ProductManager {
       code,
       stock
     })
-    return console.log('Product added correctly!')
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2))
+      console.log('Product added correctly!')
+    }
+    catch(err) {
+      console.log(err)
+    }
   }
 
-  getProducts() {
-    return console.log(this.products)
+  getProducts = async () => {
+    const collectionData = await ProductManager.getCollection(this.path)
+    console.log(collectionData)
   }
 
-  getProductById(id) {
-    const product = this.products.find(product => product.id === id)
+  getProductById = async (id) => {
+    const collectionData = await ProductManager.getCollection(this.path)
+    const product = collectionData.find(product => product.id === id)
     if (!product) return console.log('Not Found')
     return console.log(product)
   }
-
 }
 
-/********************************************* Testing *********************************************/
-
 // Crear colección
-const tecnology = new ProductManager
+const tecnology = new ProductManager('tecnology')
 
 // Agregar Productos
   // OK
-  console.log('TEST: Adding product #1')
   tecnology.addProduct('Headset', 'For playing music', 450, 'url', 50651, 200)
-  console.log('TEST: Adding product #2')
   tecnology.addProduct('Mouse', 'For gaming', 210, 'url', 40621, 10)
-  console.log('TEST: Adding product #3')
   tecnology.addProduct('Keyboard', 'For writing', 320, 'url', 20321, 1000)
-  console.log('------------')
   // NOK
-  console.log('TEST: Repeated code')
   tecnology.addProduct('Mouse', 'For gaming', 610, 'url', 50651, 10)
-  console.log('TEST: Missing fields')
   tecnology.addProduct('Mouse', 'For gaming')
-  console.log('------------')
 
 // Ver productos
-console.log('TEST: Get all products')
 tecnology.getProducts()
-console.log('------------')
 
 // Ver producto por ID
   // OK
-  console.log('TEST: Get a product by ID')
   tecnology.getProductById(2)
-  console.log('------------')
   // NOK 
-  console.log('TEST: Get an unexisting product by ID')
   tecnology.getProductById(8)
 
   
