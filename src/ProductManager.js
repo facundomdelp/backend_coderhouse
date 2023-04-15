5343913d6fb9f742681c6a15e7002cee13ebba55
facundomdelp <facundomdelp@gmail.com>
2023-04-15 15:32:08 -0300

    mover a nueva ruta

---
+++ src/ProductManager.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+
+class ProductManager {
+  constructor(collectionName) {
+    this.path = `./collection/${collectionName}.json`;
+    this.collectionName = collectionName;
+    this.products = [];
+    ProductManager.createCollectionFolder();
+  }
+
+  static createCollectionFolder = async () => {
+    await fs.promises.mkdir('./collection', { recursive: true });
+  };
+
+  static getCollection = async (path) => {
+    try {
+      return JSON.parse(await fs.promises.readFile(path, 'utf-8'));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  static updateCollection = async (path, products) => {
+    try {
+      await fs.promises.writeFile(path, JSON.stringify(products, null, 2));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    if (this.products.find((product) => product.code === code)) return console.log('Not valid - Repeated code');
+    const params = [title, description, price, thumbnail, code, stock];
+    if (params.includes(undefined)) return console.log('Not Valid - Enter all the fields');
+    this.products.push({
+      id: this.products[this.products.length - 1] ? this.products[this.products.length - 1].id + 1 : 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    });
+    await ProductManager.updateCollection(this.path, this.products);
+  };
+
+  getProducts = async () => {
+    const collectionData = await ProductManager.getCollection(this.path);
+    console.log(collectionData);
+    return collectionData;
+  };
+
+  getProductById = async (id) => {
+    const collectionData = await ProductManager.getCollection(this.path);
+    const product = collectionData.find((product) => product.id === id);
+    if (!product) return `${id} Not Found`;
+    console.log(product);
+    return product;
+  };
+
+  updateProduct = async (id, newTitle, newDescription, newPrice, newThumbnail, newCode, newStock) => {
+    const collectionData = await ProductManager.getCollection(this.path);
+    const indexToBeUpdated = collectionData.findIndex((product) => product.id === id);
+    if (indexToBeUpdated) {
+      if (newTitle) {
+        collectionData[indexToBeUpdated].title = newTitle;
+      }
+      if (newDescription) {
+        collectionData[indexToBeUpdated].description = newDescription;
+      }
+      if (newPrice) {
+        collectionData[indexToBeUpdated].price = newPrice;
+      }
+      if (newThumbnail) {
+        collectionData[indexToBeUpdated].thumbnail = newThumbnail;
+      }
+      if (newCode) {
+        collectionData[indexToBeUpdated].code = newCode;
+      }
+      if (newStock) {
+        collectionData[indexToBeUpdated].stock = newStock;
+      }
+      await ProductManager.updateCollection(this.path, collectionData);
+    } else {
+      console.log('The ID you want to update doesn`t exist in the database');
+    }
+  };
+
+  deleteProduct = async (id) => {
+    const collectionData = await ProductManager.getCollection(this.path);
+    const indexToBeDeleted = collectionData.findIndex((product) => product.id === id);
+    if (indexToBeDeleted !== -1) {
+      collectionData.splice(indexToBeDeleted, 1);
+      await fs.promises.unlink(this.path);
+      await ProductManager.updateCollection(this.path, collectionData);
+    } else {
+      console.log('The ID you want to delete doesn`t exist in the database');
+    }
+  };
+}
+
+module.exports = ProductManager;
---
+++ src/testing.js
@@ -0,0 +1,91 @@
+const ProductManager = require('./ProductManager');
+
+// Crear colección
+const tecnology = new ProductManager('technology');
+
+// Agregar productos
+// OK
+tecnology.addProduct('Headset Pro', 'For professional audio', 890, ['url1'], 'T-MX9', 100);
+tecnology.addProduct('Wireless Mouse', 'For easy use', 299, ['url2'], 'T-CR7', 50);
+tecnology.addProduct('Programmable Keyboard', 'For customized shortcuts', 499, ['url3'], 'T-FZ2', 200);
+tecnology.addProduct('Smart Speaker', 'With voice assistant', 1999, ['url4'], 'T-HN8', 25);
+tecnology.addProduct('Ultra-Wide Monitor', 'For super immersive experience', 3799, ['url5'], 'T-JK1', 15);
+tecnology.addProduct('Smartphone', 'For communication', 899, ['url6'], 'T-PT4', 300);
+tecnology.addProduct('Laptop', 'For working and gaming', 1399, ['url7'], 'T-GV6', 50);
+tecnology.addProduct('Amazon Echo Dot', 'For voice assistant and smart home control', 59, ['url8'], 'T-LB3', 500);
+tecnology.addProduct('Tablet', 'For entertainment', 499, ['url9'], 'T-RS7', 150);
+tecnology.addProduct('Wireless Earbuds', 'For listening', 199, ['url10'], 'T-NK5', 500);
+
+tecnology.addProduct('Gaming Chair', 'For gaming', 299, ['url11'], 'T-DJ9', 100);
+tecnology.addProduct('Gaming Mouse Pad', 'For gaming', 39, ['url12'], 'T-HP5', 300);
+tecnology.addProduct('Mechanical Keyboard', 'For gaming and typing', 149, ['url13'], 'T-QS4', 200);
+tecnology.addProduct('Smartwatch', 'For monitoring health and fitness', 199, ['url14'], 'T-VT2', 100);
+tecnology.addProduct('Fitness Tracker', 'For tracking health and fitness', 99, ['url15'], 'T-ZG1', 500);
+tecnology.addProduct('Bluetooth Speaker', 'For listening to music', 99, ['url16'], 'T-FL6', 300);
+tecnology.addProduct('External Hard Drive', 'For storage', 129, ['url17'], 'T-KN5', 200);
+tecnology.addProduct('Webcam', 'For video conferencing', 79, ['url18'], 'T-YW1', 500);
+tecnology.addProduct('Microphone', 'For recording audio', 99, ['url19'], 'T-AB8', 300);
+tecnology.addProduct('Portable Charger', 'For charging devices on the go', 49, ['url20'], 'T-XD4', 500);
+
+tecnology.addProduct('Wireless Charger', 'For charging devices without cables', 29, ['url21'], 'T-UF2', 1000);
+tecnology.addProduct('Monitor', 'For computing and gaming', 399, ['url22'], 'T-QR8', 50);
+tecnology.addProduct('Virtual Reality Headset', 'For immersive experiences', 499, ['url23'], 'T-LM5', 100);
+tecnology.addProduct('Drone', 'For aerial photography and videography', 599, ['url24'], 'T-GH8', 50);
+tecnology.addProduct('Action Camera', 'For recording action footage', 299, ['url25'], 'T-DF2', 100);
+tecnology.addProduct('Smart Speaker', 'For playing music and answering questions', 99, ['url26'], 'T-KL3', 500);
+tecnology.addProduct('Smart Plug', 'For controlling home devices with a mobile app', 29, ['url27'], 'T-WR9', 1000);
+tecnology.addProduct('Smart Light Bulb', 'For controlling lighting with a mobile app', 19, ['url28'], 'T-QS7', 1000);
+tecnology.addProduct('Smart Thermostat', 'For controlling home temperature with a mobile app', 249, ['url29'], 'T-TY5', 50);
+tecnology.addProduct('Robotic Vacuum', 'For automatic cleaning', 399, ['url30'], 'T-JK6', 50);
+
+tecnology.addProduct('Gaming Headset', 'For immersive gaming audio', 599, ['url31'], 'T-PL4', 100);
+tecnology.addProduct('Ergonomic Mouse', 'For comfortable use', 249, ['url32'], 'T-DC1', 100);
+tecnology.addProduct('Wireless Keyboard', 'For convenient typing', 399, ['url33'], 'T-LM9', 75);
+tecnology.addProduct('Portable Bluetooth Speaker', 'For on-the-go audio', 149, ['url34'], 'T-OP2', 200);
+tecnology.addProduct('Ultra-Thin Monitor', 'For sleek design', 1999, ['url35'], 'T-BN0', 10);
+tecnology.addProduct('iPhone', 'For communication and multimedia', 1299, ['url36'], 'T-AX6', 150);
+tecnology.addProduct('Macbook', 'For professional use', 2499, ['url37'], 'T-WE1', 25);
+tecnology.addProduct('iPad Pro', 'For creative work and entertainment', 1199, ['url38'], 'T-RT3', 50);
+tecnology.addProduct('Wireless Noise-Cancelling Earbuds', 'For high-quality listening', 349, ['url39'], 'T-VC8', 300);
+tecnology.addProduct('Racing Gaming Chair', 'For ergonomic gaming posture', 499, ['url40'], 'T-HG4', 75);
+
+tecnology.addProduct('Gaming Keyboard', 'For responsive gaming inputs', 179, ['url41'], 'T-FK2', 200);
+tecnology.addProduct('Gaming Mouse', 'For precise gaming movements', 99, ['url42'], 'T-MJ5', 300);
+tecnology.addProduct('Fitbit', 'For fitness tracking', 199, ['url43'], 'T-UY7', 100);
+tecnology.addProduct('Garmin Watch', 'For outdoor activities tracking', 399, ['url44'], 'T-NB1', 75);
+tecnology.addProduct('Bluetooth Headphones', 'For wireless audio', 149, ['url45'], 'T-PQ9', 150);
+tecnology.addProduct('SSD', 'For high-speed storage', 199, ['url46'], 'T-AB4', 100);
+tecnology.addProduct('USB Microphone', 'For high-quality audio recording', 79, ['url47'], 'T-CC2', 200);
+tecnology.addProduct('Power Bank', 'For portable device charging', 29, ['url48'], 'T-BD1', 500);
+tecnology.addProduct('Wireless Charging Pad', 'For convenient device charging', 19, ['url49'], 'T-EE3', 1000);
+tecnology.addProduct('Curved Monitor', 'For immersive display', 899, ['url50'], 'T-FF2', 25);
+
+tecnology.addProduct('Oculus Quest 2', 'For wireless VR gaming', 399, ['url51'], 'T-GG1', 50);
+tecnology.addProduct('DJI Mavic Pro 2', 'For high-quality aerial photography and videography', 1499, ['url52'], 'T-AB2', 10);
+tecnology.addProduct('GoPro Hero 10', 'For high-quality action footage', 499, ['url53'], 'T-CC3', 100);
+tecnology.addProduct('Wireless Noise-Cancelling Earbuds', 'For immersive audio experience', 299, ['url54'], 'T-BD4', 100);
+tecnology.addProduct('Smart Scale', 'For tracking weight and body composition', 79, ['url55'], 'T-EE1', 250);
+tecnology.addProduct('Robot Lawn Mower', 'For automatic lawn care', 899, ['url56'], 'T-FF3', 25);
+tecnology.addProduct('Drone with Camera', 'For aerial photography and videography', 999, ['url56'], 'T-GG2', 50);
+tecnology.addProduct('Wireless Gaming Controller', 'For comfortable gaming', 79, ['url58'], 'T-AB1', 200);
+tecnology.addProduct('Bluetooth Headphones', 'For listening to music and taking calls', 129, ['url59'], 'T-CC4', 150);
+tecnology.addProduct('Smart Home Security Camera', 'For monitoring home security', 199, ['url60'], 'T-BD2', 100);
+
+/* // NOK
+tecnology.addProduct('Mouse', 'For gaming', 610, ['url'], 'T-MX9', 10);
+tecnology.addProduct('Mouse', 'For gaming');
+
+// Ver productos
+tecnology.getProducts();
+
+// Ver producto por ID
+// OK
+tecnology.getProductById(2);
+// NOK
+tecnology.getProductById(8);
+
+// Actualizar producto por ID
+tecnology.updateProduct(3, 'Headset', 'For playing music', 600);
+
+// Eliminar producto por ID
+tecnology.deleteProduct(61); */
