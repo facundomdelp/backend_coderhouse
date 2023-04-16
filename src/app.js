d0c237c3370554dc9f9e411ba0e7eae52e7b7a62
facundomdelp <facundomdelp@gmail.com>
2023-04-16 18:48:01 -0300

    desarrollar archivo raíz

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const router = require('./router/products.js');
+const productsRouter = require('./router/products');
+const cartsRouter = require('./router/carts');
 
 const PORT = 8080;
-const server = express();
 
+const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use('/api', router);
+server.get('/', (req, res) => {
+  res.send(`Welcome to Facundo's Market Place SERVER</br></br>Route /api for more options`);
+});
+
+server.get('/api', (req, res) => {
+  res.send(`Route /products</br>Route /carts`);
+});
+
+server.use('/api', productsRouter);
+server.use('/api', cartsRouter);
 server.use('/public', express.static(`${__dirname}/public`));
 
 server.listen(PORT, () => {
