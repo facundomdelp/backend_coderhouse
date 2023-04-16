ba240b450656e477e18d7f45ca01cb48aae3b217
facundomdelp <facundomdelp@gmail.com>
2023-04-16 18:48:32 -0300

    mejorar product manager para persistencia

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -3,15 +3,26 @@ const fs = require('fs');
 class ProductManager {
   constructor() {
     this.path = '../data/products.json';
-    this.products = [];
     ProductManager.createDataFolder();
+    this.productsInit();
   }
 
   static createDataFolder = async () => {
     await fs.promises.mkdir('../data', { recursive: true });
   };
 
-  static updateCollection = async (path, products) => {
+  static loadProducts = async (path) => {
+    try {
+      if (!fs.existsSync(path)) {
+        await fs.promises.writeFile(path, JSON.stringify([]));
+      }
+      return JSON.parse(await fs.promises.readFile(path, 'utf-8'));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  static saveProducts = async (path, products) => {
     try {
       await fs.promises.writeFile(path, JSON.stringify(products, null, 2));
     } catch (err) {
@@ -19,20 +30,31 @@ class ProductManager {
     }
   };
 
+  productsInit = async () => {
+    this.products = (await ProductManager.loadProducts(this.path)) || [];
+  };
+
   getProducts = async () => {
-    try {
-      const products = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'));
-      console.log(products);
-      return products;
-    } catch (err) {
-      console.log(err);
-    }
+    const products = await ProductManager.loadProducts(this.path);
+    return products;
+  };
+
+  getProductById = async (id) => {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) throw new Error(`ID ${id} Not Found`);
+    return product;
   };
 
   addProduct = async ({ title, description, code, price, stock, category, thumbnails }) => {
-    if (this.products.find((product) => product.code === code)) return console.log(`${title} - ${code} Not valid - Repeated code`);
+    if (this.products.find((product) => product.code === code)) {
+      throw new Error(`${title} - ${code} Not valid - Repeated code`);
+    }
+
     const params = [title, description, code, price, stock, category];
-    if (params.includes(undefined)) return console.log(`${params[0]} - ID ${params[2]} - Not Valid - Enter all the mandatory fields`);
+    if (params.includes(undefined)) {
+      throw new Error(`${params[0]} - ID ${params[2]} - Not Valid - Enter all the mandatory fields`);
+    }
+
     this.products.push({
       id: this.products[this.products.length - 1] ? this.products[this.products.length - 1].id + 1 : 1,
       title,
@@ -44,44 +66,44 @@ class ProductManager {
       category,
       thumbnails,
     });
-    await ProductManager.updateCollection(this.path, this.products);
-  };
 
-  getProductById = async (id) => {
-    const products = await ProductManager.getProducts();
-    const product = products.find((product) => product.id === id);
-    if (!product) return `ID ${id} Not Found`;
-    console.log(product);
-    return product;
+    await ProductManager.saveProducts(this.path, this.products);
+    return { message: `Product ${title} - ${code} added successfully` };
   };
 
   updateProduct = async ({ id, title, description, code, price, stock, category, thumbnails }) => {
-    const products = await ProductManager.getProducts();
+    const products = await ProductManager.loadProducts(this.path);
     const indexToUpdate = products.findIndex((product) => product.id === id);
-    if (indexToUpdate ?? false) {
-      const productToUpdate = products[indexToUpdate];
-      productToUpdate.title = title ?? productToUpdate.title;
-      productToUpdate.description = description ?? productToUpdate.description;
-      productToUpdate.code = code ?? productToUpdate.code;
-      productToUpdate.price = price ?? productToUpdate.price;
-      productToUpdate.stock = stock ?? productToUpdate.stock;
-      productToUpdate.category = category ?? productToUpdate.category;
-      productToUpdate.thumbnails = thumbnails ?? productToUpdate.thumbnails;
-      await ProductManager.updateCollection(this.path, products);
-    } else {
-      console.log(`The ID ${id} you want to update doesn't exist in the database`);
+
+    if (indexToUpdate === -1) {
+      throw new Error(`The ID ${id} you want to update doesn't exist in the database`);
     }
+
+    const productToUpdate = products[indexToUpdate];
+    productToUpdate.title = title ?? productToUpdate.title;
+    productToUpdate.description = description ?? productToUpdate.description;
+    productToUpdate.code = code ?? productToUpdate.code;
+    productToUpdate.price = price ?? productToUpdate.price;
+    productToUpdate.stock = stock ?? productToUpdate.stock;
+    productToUpdate.category = category ?? productToUpdate.category;
+    productToUpdate.thumbnails = thumbnails ?? productToUpdate.thumbnails;
+
+    await ProductManager.saveProducts(this.path, products);
+    return { message: `Product ${title} - ${code} updated successfully` };
   };
 
   deleteProduct = async (id) => {
-    const products = await ProductManager.getProducts();
+    const products = await ProductManager.loadProducts(this.path);
     const indexToBeDeleted = products.findIndex((product) => product.id === id);
-    if (indexToBeDeleted !== -1) {
-      products.splice(indexToBeDeleted, 1);
-      await ProductManager.updateCollection(this.path, products);
-    } else {
-      console.log(`The ID ${id} you want to update doesn't exist in the database`);
+    const productToBeDeleted = products[indexToBeDeleted];
+
+    if (indexToBeDeleted === -1) {
+      throw new Error(`The ID ${id} you want to update doesn't exist in the database`);
     }
+
+    products.splice(indexToBeDeleted, 1);
+    await ProductManager.saveProducts(this.path, products);
+    return { message: `Product ${productToBeDeleted.title} - ${productToBeDeleted.code} deleted successfully` };
   };
 }
 
