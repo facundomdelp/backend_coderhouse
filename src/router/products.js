3b7888f855becad3665083a067f3be48657c3374
facundomdelp <facundomdelp@gmail.com>
2023-04-16 18:48:42 -0300

    desarrollar routers

--- src/router/products.js
+++ src/router/products.js
@@ -2,24 +2,47 @@ const express = require('express');
 const ProductManager = require('../ProductManager');
 
 const router = express.Router();
+const productManager = new ProductManager();
 
-router.get('/', (req, res) => {
-  res.send(`Welcome to the Facundo's Market Place`);
-});
-
-router.get('/:collection', (req, res) => {
-  res.send(`Welcome to the ${req.params.collection} collection`);
-});
-
-router.get('/:collection/products', async (req, res) => {
-  const fileManager = new ProductManager(`${req.params.collection}`);
-  const products = await fileManager.getProducts();
+router.get('/products', async (req, res) => {
+  const products = await productManager.getProducts();
   const exposedProducts = products.slice(0, req.query.limit ? req.query.limit : products.length);
   res.send(JSON.stringify(exposedProducts));
 });
 
-router.get('/:collection/products/:id', async (req, res) => {
-  const fileManager = new ProductManager(`${req.params.collection}`);
-  const product = await fileManager.getProductById(parseInt(req.params.id));
-  res.send(JSON.stringify(product));
+router.get('/products/:pid', async (req, res) => {
+  const product = await productManager.getProductById(parseInt(req.params.pid));
+  if (!product) {
+    return res.status(404).send();
+  }
+  res.status(200).send(JSON.stringify(product));
 });
+
+router.post('/products', async (req, res) => {
+  try {
+    const result = await productManager.addProduct(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.put('/products/:pid', async (req, res) => {
+  try {
+    const result = await productManager.updateProduct({ id: parseInt(req.params.pid), ...req.body });
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.delete('/products/:pid', async (req, res) => {
+  try {
+    const result = await productManager.deleteProduct(parseInt(req.params.pid));
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+module.exports = router;
