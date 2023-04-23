0682b43ceb15e35cb019027f248a9710cfab9e8d
facundomdelp <facundomdelp@gmail.com>
2023-04-23 19:36:03 -0300

    develop handlebars with websocket

---
+++ src/router/views.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import ProductManager from '../controllers/ProductManager.js';
+
+const router = express.Router();
+const productManager = new ProductManager();
+
+router.get('/products', async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render('home', {
+    products,
+  });
+});
+
+router.get('/realtimeproducts', async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render('realTimeProducts', {
+    products,
+  });
+});
+
+export default router;
