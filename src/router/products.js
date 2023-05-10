1a5ddda57c8d85d35b64b7e6a529dfed608f25e1
facundomdelp <facundomdelp@gmail.com>
2023-05-10 18:22:39 -0300

    set productsRouter as a function

--- src/router/products.js
+++ src/router/products.js
@@ -1,54 +1,57 @@
 import express from 'express';
-import ProductManager from '../controllers/ProductManager.js';
-import { wss } from './../app.js';
+import ProductManager from '../dao/ProductManager.js';
 
 const router = express.Router();
 const productManager = new ProductManager();
 
-router.get('/products', async (req, res) => {
-  const products = await productManager.getProducts();
-  const exposedProducts = products.slice(0, req.query.limit ? req.query.limit : products.length);
-  res.send(JSON.stringify(exposedProducts));
-});
+const productsRouter = (wss) => {
+  router.get('/products', async (req, res) => {
+    const products = await productManager.getProducts();
+    const exposedProducts = products.slice(0, req.query.limit ? req.query.limit : products.length);
+    res.send(JSON.stringify(exposedProducts));
+  });
 
-router.get('/products/:pid', async (req, res) => {
-  const product = await productManager.getProductById(parseInt(req.params.pid));
-  if (!product) {
-    return res.status(404).send();
-  }
-  res.status(200).send(JSON.stringify(product));
-});
+  router.get('/products/:pid', async (req, res) => {
+    const product = await productManager.getProductById(parseInt(req.params.pid));
+    if (!product) {
+      return res.status(404).send();
+    }
+    res.status(200).send(JSON.stringify(product));
+  });
 
-router.post('/products', async (req, res) => {
-  try {
-    const newProduct = req.body;
-    const message = await productManager.addProduct(newProduct);
-    const newId = message.id;
-    res.status(200).send(message);
-    wss.emit('new_product', { message, newProduct, newId });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-});
+  router.post('/products', async (req, res) => {
+    try {
+      const newProduct = req.body;
+      const message = await productManager.addProduct(newProduct);
+      const newId = message.id;
+      res.status(200).send(message);
+      wss.emit('new_product', { message, newProduct, newId });
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
+  });
 
-router.put('/products/:pid', async (req, res) => {
-  try {
-    const message = await productManager.updateProduct({ id: parseInt(req.params.pid), ...req.body });
-    res.status(200).send(message);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-});
+  router.put('/products/:pid', async (req, res) => {
+    try {
+      const message = await productManager.updateProduct({ id: parseInt(req.params.pid), ...req.body });
+      res.status(200).send(message);
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
+  });
 
-router.delete('/products/:pid', async (req, res) => {
-  try {
-    const deletedId = req.params.pid;
-    const message = await productManager.deleteProduct(parseInt(deletedId));
-    res.status(200).send(message);
-    wss.emit('deleted_product', { message, deletedId });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-});
+  router.delete('/products/:pid', async (req, res) => {
+    try {
+      const deletedId = req.params.pid;
+      const message = await productManager.deleteProduct(parseInt(deletedId));
+      res.status(200).send(message);
+      wss.emit('deleted_product', { message, deletedId });
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
+  });
 
-export default router;
+  return router;
+};
+
+export default productsRouter;
