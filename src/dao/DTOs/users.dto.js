9e5b88310261917ffd514ff6e7e7cfba2aaa2bfb
facundomdelp <facundomdelp@gmail.com>
2023-07-08 19:14:08 -0300

    fix

--- src/dao/DTOs/users.dto.js
+++ src/dao/DTOs/users.dto.js
@@ -1,10 +1,12 @@
 class UsersDTO {
   constructor(user) {
-    // Se filtran los datos sensibles
+    // Se envían solo los datos necesarios y se evita envíar datos sensibles al front
     const { first_name, last_name, email, age, password, role, cart } = user;
     this.first_name = first_name;
     this.email = email;
     this.cartId = cart;
+    this.role = role;
+    const filteredData = { last_name, age, password };
   }
 }
 
