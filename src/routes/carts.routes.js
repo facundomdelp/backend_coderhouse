9d794bfc17783ef5b5f5779e708085ae6a1a86df
facundomdelp <facundomdelp@gmail.com>
2023-07-08 18:36:46 -0300

    manage authorizations

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -9,12 +9,13 @@ import {
   updateProductQuantityFromCart
 } from '../controllers/carts.controllers.js';
 import { apiValidate } from '../middlewares/validation.js';
+import { userAuthorization } from '../middlewares/authorization.js';
 
 const router = express.Router();
 
 router.post('/', apiValidate, createCart);
 router.get('/:cid', apiValidate, getCartById);
-router.post('/:cid/products/:pid', apiValidate, addProductToCart);
+router.post('/:cid/products/:pid', apiValidate, userAuthorization, addProductToCart);
 router.delete('/:cid/products/:pid', apiValidate, deleteProductFromCart);
 router.put('/:cid', apiValidate, updateCart);
 router.put('/:cid/products/:pid', apiValidate, updateProductQuantityFromCart);
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import { addProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/products.controllers.js';
 import { apiValidate } from '../middlewares/validation.js';
+import { adminAuthorization } from '../middlewares/authorization.js';
 
 const router = express.Router();
 
 const productsRouter = (wss) => {
   router.get('/', apiValidate, getProducts);
   router.get('/:pid', apiValidate, getProductById);
-  router.post('/', apiValidate, (req, res) => addProduct(req, res, wss));
-  router.put('/:pid', apiValidate, updateProduct);
-  router.delete('/:pid', apiValidate, (req, res) => deleteProduct(req, res, wss));
+  router.post('/', apiValidate, adminAuthorization, (req, res) => addProduct(req, res, wss));
+  router.put('/:pid', apiValidate, adminAuthorization, updateProduct);
+  router.delete('/:pid', apiValidate, adminAuthorization, (req, res) => deleteProduct(req, res, wss));
 
   return router;
 };
